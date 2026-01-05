c3e08af714976f2e557a1925487b969fb99e2a90
Taynerd <taynacaires2004@gmail.com>
2026-01-04 21:10:06 -0300

    atualizacao

--- inicial.js
+++ inicial.js
@@ -1,4 +1,35 @@
-document.getElementById("menu").addEventListener("click", function (abrirMenu) {
-  const menu = document.getElementById("area-menu");
-  menu.classList.toggle("active");
+const menuBtn = document.getElementById("menu");
+const areaMenu = document.getElementById("area-menu");
+const overlay = document.getElementById("menu-overlay");
+
+// abrir / fechar menu
+menuBtn.addEventListener("click", () => {
+  areaMenu.classList.toggle("active");
+  overlay.classList.toggle("active");
 });
+
+// fechar ao clicar fora
+overlay.addEventListener("click", fecharMenu);
+
+// fechar ao clicar em link
+document.querySelectorAll(".area-menu a").forEach(link => {
+  link.addEventListener("click", fecharMenu);
+});
+
+function fecharMenu() {
+  areaMenu.classList.remove("active");
+  overlay.classList.remove("active");
+}
+
+// REMOVE ELASTIC SCROLL NO IOS
+document.addEventListener(
+  'touchmove',
+  (e) => {
+    if (areaMenu.classList.contains('active')) {
+      if (!e.target.closest('.area-menu')) {
+        e.preventDefault();
+      }
+    }
+  },
+  { passive: false }
+);
